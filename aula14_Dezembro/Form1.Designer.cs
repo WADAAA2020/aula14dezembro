38c4dee83ff9100176ccff5631f86856023518b6
WADAAA2020 <87211924+WADAAA2020@users.noreply.github.com>
2022-12-14 11:50:36 -0300

    Add files via upload

---
+++ aula14_Dezembro/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace aula14_Dezembro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputSenha = new System.Windows.Forms.TextBox();
+            this.labelMensagem = new System.Windows.Forms.Label();
+            this.buttonLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputSenha
+            // 
+            this.inputSenha.Location = new System.Drawing.Point(119, 167);
+            this.inputSenha.Name = "inputSenha";
+            this.inputSenha.Size = new System.Drawing.Size(204, 20);
+            this.inputSenha.TabIndex = 0;
+            this.inputSenha.TextChanged += new System.EventHandler(this.inputSenha_TextChanged);
+            this.inputSenha.KeyDown += new System.Windows.Forms.KeyEventHandler(this.VerificaCaracter);
+            // 
+            // labelMensagem
+            // 
+            this.labelMensagem.AutoSize = true;
+            this.labelMensagem.Location = new System.Drawing.Point(116, 242);
+            this.labelMensagem.Name = "labelMensagem";
+            this.labelMensagem.Size = new System.Drawing.Size(35, 13);
+            this.labelMensagem.TabIndex = 1;
+            this.labelMensagem.Text = "label1";
+            // 
+            // buttonLimpar
+            // 
+            this.buttonLimpar.BackColor = System.Drawing.Color.Black;
+            this.buttonLimpar.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLimpar.ForeColor = System.Drawing.Color.Lime;
+            this.buttonLimpar.Location = new System.Drawing.Point(367, 139);
+            this.buttonLimpar.Name = "buttonLimpar";
+            this.buttonLimpar.Size = new System.Drawing.Size(140, 48);
+            this.buttonLimpar.TabIndex = 2;
+            this.buttonLimpar.Text = "Limpar";
+            this.buttonLimpar.UseVisualStyleBackColor = false;
+            this.buttonLimpar.Click += new System.EventHandler(this.buttonLimpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 478);
+            this.Controls.Add(this.buttonLimpar);
+            this.Controls.Add(this.labelMensagem);
+            this.Controls.Add(this.inputSenha);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputSenha;
+        private System.Windows.Forms.Label labelMensagem;
+        private System.Windows.Forms.Button buttonLimpar;
+    }
+}
+
---
+++ aula14_Dezembro/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aula14_Dezembro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class ChecaForcaSenha
+        {
+
+            public enum ForcaDaSenha
+            {
+                Inaceitavel,
+                Fraca,
+                Aceitavel,
+                Forte,
+                Segura
+            }
+
+            public int geraPontosSenha(string senha)
+            {
+                if (senha == null) return 0;
+                int pontosPorTamanho = GetPontoPorTamanho(senha);
+                int pontosPorMinusculas = GetPontoPorMinusculas(senha);
+                int pontosPorMaiusculas = GetPontoPorMaiusculas(senha);
+                int pontosPorDigitos = GetPontoPorDigitos(senha);
+                int pontosPorSimbolos = GetPontoPorSimbolos(senha);
+                int pontosPorRepeticao = GetPontoPorRepeticao(senha);
+                return pontosPorTamanho + pontosPorMinusculas +
+                    pontosPorMaiusculas + pontosPorDigitos +
+                    pontosPorSimbolos - pontosPorRepeticao;
+            }
+
+            private int GetPontoPorTamanho(string senha)
+            {
+                return Math.Min(10, senha.Length) * 7;
+            }
+
+            private int GetPontoPorMinusculas(string senha)
+            {
+                int rawplacar = senha.Length - Regex.Replace(senha, "[a-z]", "").Length;
+                return Math.Min(2, rawplacar) * 5;
+            }
+
+            private int GetPontoPorMaiusculas(string senha)
+            {
+                int rawplacar = senha.Length - Regex.Replace(senha, "[A-Z]", "").Length;
+                return Math.Min(2, rawplacar) * 5;
+            }
+
+            private int GetPontoPorDigitos(string senha)
+            {
+                int rawplacar = senha.Length - Regex.Replace(senha, "[0-9]", "").Length;
+                return Math.Min(2, rawplacar) * 6;
+            }
+
+            private int GetPontoPorSimbolos(string senha)
+            {
+                int rawplacar = Regex.Replace(senha, "[a-zA-Z0-9]", "").Length;
+                return Math.Min(2, rawplacar) * 5;
+            }
+
+            private int GetPontoPorRepeticao(string senha)
+            {
+                System.Text.RegularExpressions.Regex regex = new System.Text.RegularExpressions.Regex(@"(\w)*.*\1");
+                bool repete = regex.IsMatch(senha);
+                if (repete)
+                {
+                    return 30;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+
+            public ForcaDaSenha GetForcaDaSenha(string senha)
+            {
+                int placar = geraPontosSenha(senha);
+
+                if (placar < 50)
+                    return ForcaDaSenha.Inaceitavel; //<50 inaceitavel
+                else if (placar < 60)
+                    return ForcaDaSenha.Fraca; //<60 fraca
+                else if (placar < 80)
+                    return ForcaDaSenha.Aceitavel; //<80 aceitavel
+                else if (placar < 100)
+                    return ForcaDaSenha.Forte; //<100 forte
+                else
+                    return ForcaDaSenha.Segura;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonLimpar_Click(object sender, EventArgs e)
+        {
+            inputSenha.Text = ""; // apaga a senha
+            labelMensagem.Text = ""; // apaga a mensagem 
+        }
+
+        private void inputSenha_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void VerificaCaracter(object sender, KeyEventArgs e)
+        {
+            ChecaForcaSenha verifica = new ChecaForcaSenha(); // classe,objeto que estara na classe,construtor da classe
+            ChecaForcaSenha.ForcaDaSenha forca; // todas as categorias das senhas,variavel guarda uns dos valores
+            forca = verifica.GetForcaDaSenha(inputSenha.Text); // todos os niveis de senha 
+            labelMensagem.Text = forca.ToString(); // variavel guardada a categoria,representacao textual de um objeto
+
+        }
+    }
+}
